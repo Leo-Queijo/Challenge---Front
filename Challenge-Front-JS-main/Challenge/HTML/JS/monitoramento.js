fc9908517d75e13963632cb37dccca4c54043bdb
Leo Queijo <leoqueijo8@hotmail.com>
2025-04-29 17:18:23 -0300

    Delete Challenge-Front-JS-main/Challenge/HTML directory

--- Challenge-Front-JS-main/Challenge/HTML/JS/monitoramento.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('nutritionForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    // Salvar ou processar os dados aqui
-    alert('Dados nutricionais salvos com sucesso!');
-});
